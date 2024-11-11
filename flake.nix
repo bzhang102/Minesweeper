1a6e82a93b5bc41f6c52c269579616d5552be3d1
bzhang102 <yuxiangbrandonzhang@gmail.com>
2024-11-10 16:00:40 -0800

    skeleton

--- flake.nix
+++ flake.nix
@@ -3,20 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    { self, nixpkgs }:
-    let
-      pkgs = nixpkgs.legacyPackages."x86_64-linux";
-    in
-    {
-      devShells."x86_64-linux".default = pkgs.mkShell {
-        packages = [ pkgs.nodejs ];
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ 
+            nodejs
+          ];
 
-        shellHook = ''
-          echo "Hello Devs"
-        '';
-      };
-    };
+          shellHook = ''
+            echo "Hello Devs"
+          '';
+        };
+      }
+    );
 }
