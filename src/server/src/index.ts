4f5a283739732c4953a9570910dec371b4ca305a
Nakul Joshi <nakulsaliljoshi@gmail.com>
2024-12-02 16:19:48 -0800

    fixed issue with server types

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -1,11 +1,19 @@
 // src/server/src/index.ts
-import express from "express";
+import express, { Request, Response } from "express";
 import { Socket, Server } from "socket.io";
 import { v4 as uuidv4 } from "uuid";
 import http from "http";
 import { GameState } from "./game/GameState";
 import { Coord } from "./types/gameTypes";
-import { User, Dictionary } from "./types/serverTypes";
+import {
+  LobbyState,
+  User,
+  Dictionary,
+  ClientToServerEvents,
+  ServerToClientEvents,
+  CheckLobbyRequest,
+  CreateLobbyRequest,
+} from "./types/serverTypes";
 import cors from "cors";
 import { Client } from 'pg';
 import { serverConfig } from "./config";
@@ -21,8 +29,9 @@ const corsOptions = {
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization']
 };
-app.options('*', cors());
 // Body parsing middleware
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -36,6 +45,15 @@ client.connect()
   .then(() => console.log('Connected to PostgreSQL database'))
   .catch(err => console.error('Error connecting to database:', err));
 
+
+  // Validate room ID
+const isFourDigits = (room: string): boolean => {
+  return /^\d{4}$/.test(room);
+};
+
+// Main data structures for game state management
+const gameStore: Dictionary<LobbyState> = {};
+let lobbies: Set<string> = new Set();
 
 
 // Update the create account route
@@ -170,6 +188,33 @@ app.get("/get-best-time/:username", async (req, res) => {
   }
 });
 
+// Endpoint to check if a lobby exists
+app.post("/check-lobbies", (req: CheckLobbyRequest, res: Response) => {
+  const { lobby } = req.body;
+  const isInSet = lobbies.has(lobby);
+  res.json({ isInSet });
+});
+
+// Endpoint to create a new game lobby
+app.post("/create-lobby", (req: CreateLobbyRequest, res: Response) => {
+  const { gameConfig, room } = req.body;
+  console.log(`Creating Room ${room}`);
+
+  // Initialize game state for the new room
+  gameStore[room] = {
+    board: new GameState(gameConfig),
+    config: gameConfig,
+    users: {},
+    connections: {},
+  };
+  lobbies.add(room);
+  res.send({
+    status: "ok",
+    message: "Minesweeper lobby generated",
+    timestamp: new Date().toISOString(),
+  });
+});
+
 async function findPersonByUsernameAndPassword(username:string, password: string) {
   const query = `SELECT * FROM persons WHERE username = $1 AND userpassword = $2`;
 
@@ -247,20 +292,28 @@ let globalTimerInterval: NodeJS.Timeout | null = null;
 const connections: Dictionary<Socket> = {};
 const users: Dictionary<User> = {};
 
-const handleMovement = (cursorPosition: User["state"], uuid: string) => {
-  users[uuid] = {
-    ...users[uuid],
+// Handler for cursor movement updates
+const handleMovement = (
+  cursorPosition: User["state"],
+  uuid: string,
+  room: string
+): void => {
+  // Update user's cursor position in game state
+  gameStore[room].users[uuid] = {
+    ...gameStore[room].users[uuid],
     state: cursorPosition,
   };
-  io.emit("users", users);
+
+  // Broadcast updated user positions to all clients in room
+  io.to(room).emit("users", gameStore[room].users);
 };
 
-const handleClose = (uuid: string) => {
-  console.log(`Disconnecting ${users[uuid].username}`);
-  delete connections[uuid];
-  delete users[uuid];
-  console.log(users);
-  io.emit("users", users);
+// Handler for closed connection
+const handleClose = (uuid: string, room: string): void => {
+  console.log(`Disconnecting ${gameStore[room].users[uuid].uuid}`);
+  delete gameStore[room].connections[uuid];
+  delete gameStore[room].users[uuid];
+  io.to(room).emit("users", gameStore[room].users);
 };
 
 // Function to start timer if it's not already running
@@ -285,59 +338,84 @@ const stopGlobalTimer = () => {
   }
 };
 
-io.on("connection", (socket) => {
-  const username = String(socket.handshake.query["username"]);
-  const uuid: string = uuidv4();
-  console.log(`${username} connected with uuid ${uuid}`);
-  
-  connections[uuid] = socket;
-  users[uuid] = {
-    uuid,
-    username,
-    state: {
-      x: -30,
-      y: -30,
-    },
-  };
+// Socket.IO connection handler
+io.on(
+  "connection",
+  (socket: Socket<ClientToServerEvents, ServerToClientEvents>) => {
+    // Extract username from connection query
+    const username = String(socket.handshake.query["username"]);
+    // Extract room ID from connection query
+    const room = String(socket.handshake.query["room"]);
+    // Generate unique ID for new user
+    const uuid: string = uuidv4();
 
-  startGlobalTimer(); // Start timer if it's not running
-  socket.emit("gameState", game.getGameState());
+    socket.join(room);
 
-  // Handle disconnect
-  socket.on("disconnect", () => {
-    handleClose(uuid);
-    stopGlobalTimer(); // Stop timer if no users left
-  });
-
-  // Handle cursor movement
-  socket.on("cursor_movement", (cursorPosition: User["state"]) =>
-    handleMovement(cursorPosition, uuid)
-  );
-
-  // Handle left clicks
-  socket.on("click", (move: Coord) => {
-    game.click(move);
-    // Broadcast new state to all players
-    io.emit("gameState", game.getGameState());
-  });
-
-  // Handle right clicks (flags)
-  socket.on("flag", (move: Coord) => {
-    game.flag(move);
-    // Broadcast new state to all players
-    io.emit("gameState", game.getGameState());
-  });
-
-  socket.on("reset", () => {
-    game = new GameState(config);
-    io.emit("gameState", game.getGameState());
-  });
-  socket.on("gameEnd", () => {
-    if (globalTimerInterval) {
-      clearInterval(globalTimerInterval);
+    if (!gameStore[room]) {
+      console.log(`Room ${room} does not exist. Disconnecting client.`);
+      socket.emit("error", "Room does not exist");
+      socket.disconnect(true);
+      return;
     }
+
+    // Get reference to game instance for this room
+    let game = gameStore[room].board;
+
+    console.log(`User connected with uuid ${uuid} to room ${room}`);
+    // Store user's connection and initialize their state as off screen
+    gameStore[room].connections[uuid] = socket;
+    gameStore[room].users[uuid] = {
+      uuid,
+      username,
+      state: {
+        x: -30,
+        y: -30,
+      },
+    };
+
+    // Send initial game state to new user
+    socket.emit("gameState", game.getGameState());
+    // startGlobalTimer(); // Start timer if it's not running
+    console.log(`UUID is ${uuid} and room is ${room}`);
+    socket.emit("uuid", uuid);
+    socket.emit("lobbies", lobbies);
+
+    // Various Event Listeners
+    socket.on("cursor_movement", (cursorPosition: User["state"]) => {
+      handleMovement(cursorPosition, uuid, room);
+    });
+
+    socket.on("disconnect", () => {
+      handleClose(uuid, room);
+      // stopGlobalTimer();
   });
-});
+
+    socket.on("click", (move: Coord) => {
+      game = gameStore[room].board;
+      game.click(move);
+      io.to(room).emit("gameState", game.getGameState());
+    });
+
+    socket.on("flag", (move: Coord) => {
+      game = gameStore[room].board;
+      game.flag(move);
+      io.to(room).emit("gameState", game.getGameState());
+    });
+
+    socket.on("reset", () => {
+      gameStore[room].board = new GameState(gameStore[room].config);
+      game = gameStore[room].board;
+      io.to(room).emit("gameState", game.getGameState());
+    });
+
+    // socket.on("gameEnd", () => {
+    //   if (globalTimerInterval) {
+    //     clearInterval(globalTimerInterval);
+    //   }
+    // });
+  }
+);
+
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
