fbe250c01b42194c034868ed34c7a483a8f89a10
bzhang102 <yuxiangbrandonzhang@gmail.com>
2024-12-03 23:37:32 -0800

    backend refactored and deployed

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -1,30 +1,4 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 16.4
--- Dumped by pg_dump version 16.4
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
---
--- Name: persons; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.persons (
+CREATE TABLE persons (
     username character varying(255),
     userpassword character varying(255),
     accesstoken character varying(255),
@@ -35,7 +9,3 @@ CREATE TABLE public.persons (
     medium_solve_partners TEXT[] DEFAULT ARRAY[]::TEXT[],
     hard_solve_partners TEXT[] DEFAULT ARRAY[]::TEXT[]
 );
-
---
--- PostgreSQL database dump complete
---
